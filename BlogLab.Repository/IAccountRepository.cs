51209319d4e53f3b8352829565a5b71c4c7df2f0
Vinay Maheshwar <m.vinay15@gmail.com>
2021-08-02 23:02:20 +0530

    Models project complete. Repository project created with Account Repository

---
+++ BlogLab.Repository/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlogLabModels.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogLab.Repository
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult>CreateAsync(ApplicationUserIdentity user,
+            CancellationToken cancellationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername,
+            CancellationToken cancellationToken);
+
+    }
+}
---
+++ BlogLabModels/Blog/PagedResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogLabModels.Blog
+{
+    public class PagedResults<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
