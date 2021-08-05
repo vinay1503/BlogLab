a2c34e84bc63019cf6335779c3f8ef93bb33e16d
Vinay Maheshwar <m.vinay15@gmail.com>
2021-08-05 12:31:42 +0530

    Generated all of the needed components

--- BlogLab-UI/src/app/app.module.ts
+++ BlogLab-UI/src/app/app.module.ts
@@ -5,11 +5,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SummaryPipe } from './pipes/summary.pipe';
+import { BlogComponent } from './blog-components/blog/blog.component';
+import { BlogCardComponent } from './components/blog-components/blog-card/blog-card.component';
+import { BlogEditComponent } from './components/blog-components/blog-edit/blog-edit.component';
+import { BlogsComponent } from './components/blog-components/blogs/blogs.component';
+import { FamousBlogsComponent } from './components/blog-components/famous-blogs/famous-blogs.component';
+import { CommentBlogsComponent } from './components/comment-components/comment-blogs/comment-blogs.component';
+import { CommentSystemComponent } from './components/comment-components/comment-system/comment-system.component';
+import { CommentsComponent } from './components/comment-components/comments/comments.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PhotoAlbumComponent } from './components/photo-album/photo-album.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryPipe
+    SummaryPipe,
+    BlogComponent,
+    BlogCardComponent,
+    BlogEditComponent,
+    BlogsComponent,
+    FamousBlogsComponent,
+    CommentBlogsComponent,
+    CommentSystemComponent,
+    CommentsComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    PhotoAlbumComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ BlogLab-UI/src/app/components/comment-components/comment-blogs/comment-blogs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-blogs',
+  templateUrl: './comment-blogs.component.html',
+  styleUrls: ['./comment-blogs.component.css']
+})
+export class CommentBlogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
